e4757b78cca2de68f0b3dbc31cd3c7f9411e1c17
Raphael Ponce <raphafleury123@gmail.com>
2020-01-31 23:17:43 -0300

    Adicionando cena de teste

---
+++ Toaster/Assets/Testes/Ponce Teste/PlayerMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+
+    public Rigidbody playerRB;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerRB.velocity = new Vector3(1f, playerRB.velocity.y, playerRB.velocity.z) * speed;
+    }
+}
