cfc0b80bca3dd844f37d7a6f7381a173350165f6
Raphael Ponce <raphafleury123@gmail.com>
2020-02-01 18:24:52 -0300

    Revert "Auto stash before merge of "master" and "origin/master""
    This reverts commit 8d2a4f75628358797b703fc6c2c873fb2dc73d15.

--- Toaster/Assets/Testes/Ponce Teste/Draggable.cs
+++ Toaster/Assets/Testes/Ponce Teste/Draggable.cs
@@ -1,24 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.EventSystems;
-
 
 public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
     public Transform parentToReturnTo = null;
     public PlayerController playerController;
-    public GameObject[] cards;
-    public GameObject handObject;
-    private int index;
-    private GameObject newCard;
-    //public Deck deck;
+    public Deck deck;
 
     public void OnBeginDrag(PointerEventData eventData)
     {
         parentToReturnTo = this.transform.parent;
-        //this.transform.SetParent(this.transform.parent.parent);
+        this.transform.SetParent(this.transform.parent.parent);
 
         //GetComponent<CanvasGroup>().blocksRaycasts = false;
     }
@@ -27,15 +21,10 @@ public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDra
     {
         Debug.Log("OnDrag");
         this.transform.position = eventData.position;
-        
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
-        index = Random.Range(0, cards.Length-1);
-        newCard = cards[index];
-        Instantiate(newCard, handObject.transform);
-
 
         this.transform.SetParent(parentToReturnTo);
 
@@ -51,35 +40,35 @@ public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDra
         {
             playerController.Jump();
             Destroy(this.gameObject);
-            //deck.usedCard = true;
+            deck.usedCard = true;
         }
 
         if (this.name == "FireCard")
         {
             playerController.Shoot();
             Destroy(this.gameObject);
-            //deck.usedCard = true;
+            deck.usedCard = true;
         }
 
         if (this.name == "GhostCard")
         {
             playerController.GhostSkill();
             Destroy(this.gameObject);
-            //deck.usedCard = true;
+            deck.usedCard = true;
         }
 
         if (this.name == "DoubleShootCard")
         {
             playerController.DoubleShot();
             Destroy(this.gameObject);
-            //deck.usedCard = true;
+            deck.usedCard = true;
         }
 
         if(this.name == "SlowTimeCard")
         {
             playerController.SlowPlayer();
             Destroy(this.gameObject);
-            //deck.usedCard = true;
+            deck.usedCard = true;
         }
     }
 }
