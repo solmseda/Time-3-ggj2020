c4b9cd42003d15560f00859ce07cea4c211848d1
Sol <solmseda@gmail.com>
2020-02-01 10:20:12 -0300

    Criação de um SceneControl básico e um scriptableObject para as cartas

---
+++ Toaster/Assets/Scripts/Cards.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class Cards : ScriptableObject{
+    public new string name;
+    public string description;
+    public Sprite artwork;
+}
